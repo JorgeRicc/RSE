e38b1cbe4e69eed1b8dec7160302dd902b84c781
Cyphar <morenomartinesteban96@gmail.com>
2023-11-19 20:01:44 -0300

    Cambios en home y cards

--- src/Pages/Frontend/Home.jsx
+++ src/Pages/Frontend/Home.jsx
@@ -1,4 +1,3 @@
-import { Link } from "react-router-dom";
 import FImageCard from "../../Components/Cards/FImageCard";
 import { FCARDS } from "../../MockData/FCards";
 
@@ -9,10 +8,8 @@ const Home = () => {
     return (
         <div className="min-h-[80vh] min-w-[100vw]">
             <div className="px-5 py-5 md:px-8 lg:px-8 grid lg:grid-cols-2 2xl:grid-cols-3 gap-x-10 gap-y-10">
-                {FCARDS.map(elem =>
-                    <Link to={String(elem.id)}>
-                        <FImageCard title={elem.title} content={elem.content} date={elem.date} image={elem.image} imageAlt={elem.imageAlt} color={elem.color} />
-                    </Link>
+                {FCARDS.map((elem, index) =>
+                    <FImageCard key={index} title={elem.title} content={elem.content} date={elem.date} image={elem.image} imageAlt={elem.imageAlt} color={elem.color} url={"/test"}/>
                 )}
             </div>
         </div>
